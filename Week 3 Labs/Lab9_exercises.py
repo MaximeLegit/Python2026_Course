8791d31767b1323f8d3f0797911b5396f375e836
Maksims Smirnovs <maksims@git.com>
2026-09-25 11:30:44 +0200

    Lab 9, part A,B,C

--- Week 3 Labs/Lab9_exercises.py
+++ Week 3 Labs/Lab9_exercises.py
@@ -50,3 +50,72 @@ engine = Engine(200)
 car = Car("Audi", engine)
 print(car.brand, car.engine.hp)
 ################################################## LECTURE ##################################################
+
+
+################################################ Assignment ##################################################
+
+# Part A. Polymorphism
+
+class Notification:
+    def send(self):
+        return "General Message"
+
+class EmailNotification(Notification):
+    def send(self):
+            return "Email Notification"
+
+class SMSNotification(Notification):
+    def send(self):
+        return "SMS Notification"
+
+notifications = [Notification(), EmailNotification(), SMSNotification()]
+
+for notification in notifications:
+    print(notification.send())
+# Why this works. This works because notification is using whatever element, be that class object, and integer or a string to do what I say it to do, in this case,
+# send the message. 
+
+
+# Part B. Polymorphism with inheritance
+
+
+class Document:
+    def __init__(self, title):
+        self.title = title
+
+    def describe(self):
+        return "The document is " + self.title
+
+class PDFDocument(Document):
+    def describe(self):
+        return "The PDF document is " + self.title
+
+class TextDocument(Document):
+    def describe(self):
+        return "The Text document is " + self.title
+
+
+documents = [PDFDocument("Aurora"), PDFDocument("Borealis"), TextDocument("Notepad"), TextDocument("++")]
+
+for document in documents:
+    print(document.describe())
+
+
+# Part C. Duck typing
+
+
+
+class Printer():
+    def display_status(self):
+        return "Printy Print"
+
+class Screen():
+    def display_status(self):
+        return "Screeny Screen"
+
+display_statuses = [Printer(), Printer(), Screen(), Screen()] # This is a bit odd, but it works. Would not get a +1 on a code review here though.
+
+for ds in display_statuses:
+    print(ds.display_status())
+# This works simple since each class is its own entity with a function called display_status(). I simply create an object and then call that function
+# that happens to have the same name, nothing more. Henec you see the printy print and screeny screen.
